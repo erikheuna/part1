c376a25bd7c5f291e40159471aa6005063e973b1
erikheuna <46852059+erikheuna@users.noreply.github.com>
2021-12-07 01:44:19 +0100

    Part1
    First Files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-unused-vars */
 
-function App() {
+const Hello = (props) => {
+  return(
+    <div>
+     <p>Hello {props.name} , you are {props.age} years old.</p>
+    </div>
+  )
+}
+/* eslint-disable no-unused-vars */
+const App = () => {
+  const name = 'Peter';
+  const age = 26;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Greetings</h1>
+      <Hello name="George" age={26 + 10} />
+      <Hello name="Allen" age={age} />
     </div>
   );
 }
