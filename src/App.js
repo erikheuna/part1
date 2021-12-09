81fe3e655bb83752d20785b3b38a1c46c3fed854
erikheuna <46852059+erikheuna@users.noreply.github.com>
2021-12-09 09:08:43 +0100

    Part1.6-1.14
    Refatoring the code for the second time

--- src/App.js
+++ src/App.js
@@ -1,54 +1,52 @@
-const Header = (props) => {
-  return(
-    <h1>{props.course}</h1>
-  )
-  
-}
+import { useState } from "react";
 
-const Content = (props) => {
+const History = (props) => {
+  if(props.allClicks.length === 0){
+    return(
+      <div>
+        <p>The app is used by pressing the buttons.</p>
+      </div>
+    )
+  }
   return(
     <div>
-    <Part name={props.part1.name} exercise={props.part1.exercises1} /> 
-    <Part name={props.part2.name} exercise={props.part2.exercises2} /> 
-    <Part name={props.part3.name} exercise={props.part2.exercises3} /> 
-  </div>
-  )
-}
-
-const Part = (props) => {
-  return(
-    <div>
-      {props.name} {props.exercise}
+      button press history: {props.allClicks.join(' ')}
     </div>
   )
 }
 
-const Total = (props) => {
+const Button = (props) => {
+  const {handleClick, text} = props;
+  console.log(props);
   return(
-    <p>Number of exercises : {props.exo1 + props.exo2 + props.exo3}</p>
+    <button onClick={handleClick}>
+   {text}
+    </button>
   )
-  
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+ 
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'));
+    setLeft(left + 1);
   }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'));
+    setRight(right + 1);
   }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+
   return (
     <div className="App">
-      <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total exo1={part1.exercises} exo2={part2.exercises} exo3={part3.exercises} />
+      {left}
+      <Button handleClick={handleLeftClick} text="left" />
+      <Button handleClick={handleRightClick} text="right" />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   );
 }
